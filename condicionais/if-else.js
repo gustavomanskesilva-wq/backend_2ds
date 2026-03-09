b44c376f7dbe33cd1c52a28cc6cb378ec317e4a6
gustavomanskesilva-wq <gustavo.manske.silva@escola.pr.gov.br>
2026-03-09 13:27:43 +0000

    introdução aos Arrays em js

---
+++ condicionais/if-else.js
@@ -0,0 +1,16 @@
+// if, else if, else, ternário
+// >, <, >=, <=, !=
+// ||,&&
+
+let idade = 120
+
+if(idade > 110){
+    console.log("Situação ímpar")
+    console.log("traga um acompanhante")
+} else if (idade >=17 && idade <= 110 ){
+    console.log("Entrada Liberada")
+    console.log("Festa sugerida: Balada!!!")
+} else {
+    console.log("Proibido, vaza menor!")
+    console.log("Festa recomendada: Monica toy")
+}
