66c06bcdcdb93bcb89588f2679be0e9758e9413e
gustavomanskesilva-wq <gustavo.manske.silva@escola.pr.gov.br>
2026-03-25 11:40:14 +0000

    finalização de obejetos

---
+++ condicionais/objetos/obj-01.js
@@ -0,0 +1,14 @@
+// Objetos são estruturas chave-valor
+// Também chamadas "key-value"
+// Objetos podem conter: arrays, string,
+// number, objects, functions...
+// possuem métodos próprios
+
+const jogador = {
+    nome: "Neymar",
+    gols: 450,
+    times: ['santos', 'psg', 'barcelona']
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.times[1])
---
+++ condicionais/objetos/obj-02.js
@@ -0,0 +1,14 @@
+// A partir do objeto de exemplo crie:
+// Um objeto com o tema á sua escolha 
+// deve possuir no minímo 6 propriedades
+// imrprima de um jeito amigável:
+// Ex.: console.log("cor do veículo: " + carro, cor)
+
+const carro = {
+    nome: "Bugatti Chiron",
+    cavaloskmh: ["'1500', '420'"],
+    cor: "azul fosco",
+}
+console.log("nome do carro" + carro.nome)
+console.log("velocidade" + carro.cavaloskmh)
+console.log("a cor do carro" + carro.cor)
