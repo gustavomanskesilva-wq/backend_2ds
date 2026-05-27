3811189b6584ba84ede2f26aed18ac6b24fe8c39
gustavomanskesilva-wq <gustavo.manske.silva@escola.pr.gov.br>
2026-05-27 10:51:09 +0000

    Finalização Módulos ESM - Petshop

---
+++ 2° TRIMESTRE/modulos/mod01/mod02.ESM/gato-mod.js
@@ -0,0 +1,15 @@
+// USA PADRÂO MODERNO IMPORT/EXPORT - ESM
+// Aqui criaremos nossos módulos
+
+export function banhoGato(raca, tipoBanho)
+{
+    return `O gato $s{raca} tomou banho: $(tipoBanho)`
+}
+export function tosaGato(raca, tipoTosa)
+{
+    return `O gato $s{raca} tomou banho: $(tipoTosa)`
+}
+export function alimentarGato(raca, tipocomida)
+{
+    return `O gato $s{raca} tomou banho: $(tipoComida)`
+}
---
+++ 2° TRIMESTRE/modulos/mod01/mod02.ESM/main-garo-mod.js
@@ -0,0 +1,7 @@
+// APENAS IMPORTA MODULOS DE 
+// OUTROS ARQUIVOS (import)
+import {alimentarGato, tosaGato, banhoGato} from `./gato-mod.js`
+
+console.log(alimentarGato('Xana', 'Petisco'))
+console.log(banhoGato('Seamês', 'Completo'))
+console.log(tosaGato('Persa', 'Bem Baixinha'))
